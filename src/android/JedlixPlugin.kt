9b7f31d3c96499b0f29b4e26ef3f6717f3d8937c
remcos <snijders.remco@gmail.com>
2023-05-31 20:10:29 +0200

    Update JedlixPlugin.kt

--- src/android/JedlixPlugin.kt
+++ src/android/JedlixPlugin.kt
@@ -1,41 +1,72 @@
-import com.jedlix.sdk.JedlixSDK
-import org.apache.cordova.CordovaPlugin
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.example.jedlixsdk.ConnectView
+import com.example.jedlixsdk.R
+import com.example.jedlixsdk.ui.ConnectSessionActivity
 
 class JedlixPlugin : CordovaPlugin() {
 
+    override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
+        if (action == "coolMethod") {
+            val userId = args.optString(0, "")
+            val accessToken = args.optString(1, "")
+            val vehicleId = args.optString(2, "")
+                                 
+            val intent = ConnectView.create(userId, accessToken, vehicleId)
+            
+            startActivity(intent)
+            
+            val result = PluginResult(PluginResult.Status.OK, "Test")
+            callbackContext.sendPluginResult(result)
+            
+            return true
+        }
+        
+        return false
+    }
+}
+
+class ConnectView {
+
     companion object {
-        lateinit var baseURL: URL
-        lateinit var apiKey: String
-        lateinit var authentication: Authentication
+        @JvmStatic
+        fun create(userId: String, accessToken: String, vehicleId: String): Intent {
+            val baseURL = "https://qa-nextenergy-smartcharging.jedlix.com"
+            val apiKey: String? = null
+            // val accessToken = "your_access_token"
+            // val userId = "your_user_id"
+            
+            val authentication = DefaultAuthentication()
+            JedlixSDK.configure(baseURL, apiKey, authentication)
+            authentication.authenticate(accessToken, userId)
+            
+            val intent = Intent(this, SomeActivity::class.java)
+            intent.putExtra("userId", userId)
+            intent.putExtra("vehicleId", vehicleId)
+            
+            return intent
+        }
     }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        baseURL = URL("<YOUR BASE URL>")
-        apiKey = "<YOUR API KEY>"
-        authentication = DefaultAuthentication(this)
-
-        JedlixSDK.configure(
-            baseURL,
-            apiKey,
-            authentication
-        )
-    }
-
 }
 
 class SomeActivity : AppCompatActivity() {
+    private lateinit var userIdentifier: String
+    private lateinit var vehicleIdentifier: String
+     
     override fun onCreate(savedInstanceState: Bundle?) {
+        userIdentifier = intent.getStringExtra("userId") ?: ""
+        vehicleIdentifier = intent.getStringExtra("vehicleId") ?: ""
+        
         val connectSessionManager = registerConnectSessionManager { result ->
             // continue when ConnectSessionActivity finishes
         }
-
-        findViewById<View>(R.id.some_button).setOnClickListener {
-            connectSessionManager.startConnectSession(
-                "<USER ID>",
-                ConnectSessionType.Vehicle
-            )
-        }
+        
+        connectSessionManager.startConnectSession(
+            userIdentifier,
+            ConnectSessionType.SelectedVehicle(vehicleIdentifier)
+        )
     }
 }
