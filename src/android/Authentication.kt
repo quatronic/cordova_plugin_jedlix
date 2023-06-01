ab6c3e8205a172970fa5037a0288791725e49841
Frank Grooten <frank.grooten@quatronic.nl>
2023-06-01 21:34:09 +0200

    Update Authentication.kt

--- src/android/Authentication.kt
+++ src/android/Authentication.kt
@@ -14,7 +14,7 @@
  *     limitations under the License.
  */
 
-package com.jedlix.sdk.example.authentication
+package com.quatronic.jedlixplugin
 
 sealed interface Authentication : com.jedlix.sdk.networking.Authentication {
     val isAuthenticated: Boolean
