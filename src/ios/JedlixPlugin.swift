ff225434982561e92d6dbc453e028dd24871b463
PetrovViktor <134078702+PetrovViktor@users.noreply.github.com>
2023-06-06 11:25:39 +0200

    Update JedlixPlugin.swift

--- src/ios/JedlixPlugin.swift
+++ src/ios/JedlixPlugin.swift
@@ -54,7 +54,7 @@ func GetToken() async throws -> OSAuthentication {
 
 
 @objc class ConnectView: NSObject {
-    @objc static func create(userId: String, vehicleId: String) -> UIViewController {
+    @objc static async func create(userId: String, vehicleId: String) -> UIViewController {
         let baseURL = URL(string: "https://demo-smartcharging.jedlix.com")!
         let apiKey: String? = nil
         let accessToken = try? await GetToken() // Await the GetToken function call
