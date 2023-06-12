2495a81d89c31a485cba222ce856d8ade6bd043d
remcos <snijders.remco@gmail.com>
2023-06-12 13:16:38 +0200

    Fixes for the Android app

--- src/android/DefaultAuthentication.kt
+++ src/android/DefaultAuthentication.kt
@@ -24,8 +24,8 @@ class DefaultAuthentication(private val context: Context) : Authentication {
     companion object {
         private const val PREFERENCE_FILE_KEY = "com.jedlix.api.example.DEFAULT_AUTHENTICATION_MANAGER"
         private const val HAS_CREDENTIALS_KEY = "HAS_CREDENTIALS"
-        private const val TOKEN_KEY = "TOKEN"
-        private const val USER_IDENTIFIER_KEY = "USER_IDENTIFIER"
+        private const val TOKEN_KEY = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5VTTJPRGxFT0VSQlJqVkNPRFEwTVRVeVJrTkdRMFF4UTBWRk9EQXpRek0wUWprelF6SkZRZyJ9.eyJpc3MiOiJodHRwczovL2plZGxpeC1iMmUuZXUuYXV0aDAuY29tLyIsInN1YiI6IjlQTjlpRzhFMDZvZDZtZzlRRTRZcHEwbTBBeDZDTFJsQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3FhLXNtYXJ0Y2hhcmdpbmcuamVkbGl4LmNvbSIsImlhdCI6MTY4NTUyNjQ4NCwiZXhwIjoxNjg2NzM2MDg0LCJhenAiOiI5UE45aUc4RTA2b2Q2bWc5UUU0WXBxMG0wQXg2Q0xSbCIsInNjb3BlIjoicmVhZDphbGwtc2Vzc2lvbnMgdXNlci1yZXNvdXJjZXMgcmVhZDphbGwtY2hhcmdpbmdsb2NhdGlvbnMgY3JlYXRlOnVzZXIgcmVhZDphbGwtdmVoaWNsZXMgcmVhZDphbGwtdXNlcnMgdGVuYW50Om5leHRlbmVyZ3kiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.QlxtpIdFHHALwg9CmU-0_0P-usuA0b_Jx4lirOnri5F_xkLYbdF6UGXW_mIK1fm4mLOIZuB6bDAUkjit1_HCITJq2tjxx2BXi2GgzLiNj8kM10jl8_mzEgAkp3ypFYeN8ItSArpkgqLYo8Y4KirYImlNqdc5Y-lwgYPCPknvwL2YNv4_diZFt8OHQQVVorFYhUDZVk0tP0ftiqvETXQFiKXyMAe_IHQ8fTMFlo-u7a0iX67ZAv1ZQqp_XaIov_C2Uemw8WvbQ0KdUFtgx55sH7xky8812EPJ4qudeN5hSkmIgpX3KfiEFGJ4IGdiYtKQ_0VGalOCzISZe44lgOobQQ"
+        private const val USER_IDENTIFIER_KEY = "161fa08b-5e5a-403e-9372-bcf5e9378451"
     }
 
     private data class Credentials(val accessToken: String, val userIdentifier: String)
