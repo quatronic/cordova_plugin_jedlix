6f4fb0a7bcd980766fdbcb0442b427ffe0b9b1e6
remcos <snijders.remco@gmail.com>
2023-06-14 17:04:59 +0200

    Test

--- src/ios/JedlixPlugin.swift
+++ src/ios/JedlixPlugin.swift
@@ -50,9 +50,10 @@ class JedlixPlugin: CDVPlugin {
          JedlixSDK.configure(baseURL: baseURL, apiKey: apiKey, authentication: authentication)
          authentication.authenticate(accessToken: accessToken, userIdentifier: userId)
 
+         callback1("Connection ended")
+
          return UIHostingController(rootView: 
-            ConnectSessionView(userIdentifier: userId, vehicleIdentifier: vehicleId) { result in
-                callback1("Connection ended")
+            ConnectSessionView(userIdentifier: userId, vehicleIdentifier: vehicleId) { result in          
                 /*
                 switch result {
                 case .notStarted: callback([state: "notStarted"])
