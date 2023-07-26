8acd5a0bf899970540673c7a1ac861fb18951743
remcos <snijders.remco@gmail.com>
2023-07-26 10:51:10 +0200

    hardcoded access token Android

--- src/android/JedlixPlugin.kt
+++ src/android/JedlixPlugin.kt
@@ -26,7 +26,7 @@ class JedlixPlugin : CordovaPlugin() {
         if (action == "coolMethod") {
             val apiKey = args.optString(0, "")
             val userId = args.optString(1, "")
-            val accessToken = args.optString(2, "")
+            val accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5VTTJPRGxFT0VSQlJqVkNPRFEwTVRVeVJrTkdRMFF4UTBWRk9EQXpRek0wUWprelF6SkZRZyJ9.eyJpc3MiOiJodHRwczovL2plZGxpeC1iMmUuZXUuYXV0aDAuY29tLyIsInN1YiI6IjlQTjlpRzhFMDZvZDZtZzlRRTRZcHEwbTBBeDZDTFJsQGNsaWVudHMiLCJhdWQiOiJodHRwczovL3FhLXNtYXJ0Y2hhcmdpbmcuamVkbGl4LmNvbSIsImlhdCI6MTY5MDE5OTE3OSwiZXhwIjoxNjkxNDA4Nzc5LCJhenAiOiI5UE45aUc4RTA2b2Q2bWc5UUU0WXBxMG0wQXg2Q0xSbCIsInNjb3BlIjoicmVhZDphbGwtc2Vzc2lvbnMgdXNlci1yZXNvdXJjZXMgcmVhZDphbGwtY2hhcmdpbmdsb2NhdGlvbnMgY3JlYXRlOnVzZXIgcmVhZDphbGwtdmVoaWNsZXMgcmVhZDphbGwtdXNlcnMgdGVuYW50Om5leHRlbmVyZ3kiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.he-8e0tc9tMxH6k-oDnknVdJm_QrvrHnvvGjLJvhrrgG4nHt9fd3fxJ92fO1OzLbojIaiWSgxvgbskhbj8CyJ5WBl8Hcce5Zxzn3FuI2LQ11HPbpxSCPg7Mr4VHM8i7t_SbtDPuY-mHAvz5BRDMpHwlGSsKrwC-L4KhZ-sr9dyGgHh-O8x_k2l_adSAGIx1QT9Bx4fzfS7ZaYjWaJoYxscSXqC1a0ch-17Lij5Mmnsv8za_pFGHOhRbQLZT6uh02gTJxfpSIHqL3uKf4hNDgiqU8xfOextJuj2pN0N0IPjXNAAJSl8Rd-omWbTJiMQEpoYJ_CSs9eEdYJXM_dyE8mA"
             val vehicleId = args.optString(3, "")
 
             var result: PluginResult
