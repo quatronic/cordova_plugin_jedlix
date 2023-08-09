0e5dbb488a3aabbc93e728f7287f717fe7d24c3d
remcos <snijders.remco@gmail.com>
2023-08-09 12:00:00 +0200

    Added the openSessionId

--- src/ios/JedlixPlugin.swift
+++ src/ios/JedlixPlugin.swift
@@ -9,8 +9,9 @@ import JedlixSDK
         let userid = command.arguments[2] as? String ?? ""
         let accesstoken = command.arguments[3] as? String ?? ""
         let vehicleid = command.arguments[4] as? String ?? ""
+        let sessionid = command.arguments[5] as? String ?? ""
         
-        let vc = ConnectView.createVehicle(url: url, apiKey: apiKey, userId: userid, accessToken: accesstoken, vehicleId: vehicleid) { result in
+        let vc = ConnectView.createVehicle(url: url, apiKey: apiKey, userId: userid, accessToken: accesstoken, vehicleId: vehicleid, openSessionId: sessionid) { result in
             let pluginResult = result[0] == "finished" ? CDVPluginResult(status: CDVCommandStatus_OK, messageAsMultipart: result) : CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsMultipart: result)
             self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
@@ -25,8 +26,9 @@ import JedlixSDK
         let userid = command.arguments[2] as? String ?? ""
         let accesstoken = command.arguments[3] as? String ?? ""
         let chargingLocationid = command.arguments[4] as? String ?? ""
+        let sessionid = command.arguments[5] as? String ?? ""
         
-        let vc = ConnectView.createCharger(url: url, apiKey: apiKey, userId: userid, accessToken: accesstoken, chargingLocationId: chargingLocationid) { result in
+        let vc = ConnectView.createCharger(url: url, apiKey: apiKey, userId: userid, accessToken: accesstoken, chargingLocationId: chargingLocationid, openSessionId: sessionid) { result in
             let pluginResult = result[0] == "finished" ? CDVPluginResult(status: CDVCommandStatus_OK, messageAsMultipart: result) : CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsMultipart: result)
             self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
@@ -37,26 +39,40 @@ import JedlixSDK
 
 
 @objc class ConnectView: NSObject {
-    @objc static func createVehicle(url: String, apiKey: String, userId: String, accessToken: String, vehicleId: String, callback: @escaping ([String]) -> Void) -> UIViewController {
+    @objc static func createVehicle(url: String, apiKey: String, userId: String, accessToken: String, vehicleId: String, openSessionId: String, callback: @escaping ([String]) -> Void) -> UIViewController {
         let baseURL = URL(string: url)!     
         let authentication = DefaultAuthentication()
 
         JedlixSDK.configure(baseURL: baseURL, apiKey: apiKey, authentication: authentication)
         authentication.authenticate(accessToken: accessToken, userIdentifier: userId)
         
-        return UIHostingController(rootView:
-            ConnectSessionView(userIdentifier: userId, vehicleIdentifier: vehicleId) { result in
-                switch result {
-                case .notStarted: callback(["notStarted"])
-                case .inProgress(let sessionId): callback(["inProgress", sessionId])
-                case .finished(let sessionId): callback(["finished", sessionId])
-                default: break
+        if (openSessionId = "") {
+            return UIHostingController(rootView:
+                ConnectSessionView(userIdentifier: userId, vehicleIdentifier: vehicleId) { result in
+                    switch result {
+                    case .notStarted: callback(["notStarted"])
+                    case .inProgress(let sessionId): callback(["inProgress", sessionId])
+                    case .finished(let sessionId): callback(["finished", sessionId])
+                    default: break
+                    }
                 }
-            }
-        )
+            )
+        }
+        else {
+            return UIHostingController(rootView:
+                ConnectSessionView(userIdentifier: userId, sessionIdentifier: openSessionId) { result in
+                    switch result {
+                    case .notStarted: callback(["notStarted"])
+                    case .inProgress(let sessionId): callback(["inProgress", sessionId])
+                    case .finished(let sessionId): callback(["finished", sessionId])
+                    default: break
+                    }
+                }
+            )
+        }      
     }
     
-    @objc static func createCharger(url: String, apiKey: String, userId: String, accessToken: String, chargingLocationId: String, callback: @escaping ([String]) -> Void) -> UIViewController {
+    @objc static func createCharger(url: String, apiKey: String, userId: String, accessToken: String, chargingLocationId: String, openSessionId: String, callback: @escaping ([String]) -> Void) -> UIViewController {
         let baseURL = URL(string: url)!
         let authentication = DefaultAuthentication()
         let type = ConnectSessionType.charger(chargingLocationId: chargingLocationId)
@@ -64,17 +80,30 @@ import JedlixSDK
         JedlixSDK.configure(baseURL: baseURL, apiKey: apiKey, authentication: authentication)
         authentication.authenticate(accessToken: accessToken, userIdentifier: userId)
 
-        return UIHostingController(rootView: 
-            ConnectSessionView(userIdentifier: userId, sessionType: type) { result in
-                switch result {
-                case .notStarted: callback(["notStarted"])
-                case .inProgress(let sessionId): callback(["inProgress", sessionId])
-                case .finished(let sessionId): callback(["finished", sessionId])
-                default: break
+        if (openSessionId = "") {
+            return UIHostingController(rootView: 
+                ConnectSessionView(userIdentifier: userId, sessionType: type) { result in
+                    switch result {
+                    case .notStarted: callback(["notStarted"])
+                    case .inProgress(let sessionId): callback(["inProgress", sessionId])
+                    case .finished(let sessionId): callback(["finished", sessionId])
+                    default: break
+                    }
                 }
-            }
-            
-        )
+            )
+        }
+        else {
+            return UIHostingController(rootView: 
+                ConnectSessionView(userIdentifier: userId, sessionIdentifier: openSessionId) { result in
+                    switch result {
+                    case .notStarted: callback(["notStarted"])
+                    case .inProgress(let sessionId): callback(["inProgress", sessionId])
+                    case .finished(let sessionId): callback(["finished", sessionId])
+                    default: break
+                    }
+                }
+            )
+        }
     }
 }
 
--- www/JedlixPlugin.js
+++ www/JedlixPlugin.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(url, apikey, userid, accesstoken, vehicleid, success, error) {
-    exec(success, error, 'JedlixPlugin', 'coolMethod', [url, apikey, userid, accesstoken, vehicleid]);
+exports.coolMethod = function(url, apikey, userid, accesstoken, vehicleid, opensessionid, success, error) {
+    exec(success, error, 'JedlixPlugin', 'coolMethod', [url, apikey, userid, accesstoken, vehicleid, opensessionid]);
 };
 
-exports.chargerMethod = function(url, apikey, userid, accesstoken, charginglocationid, success, error) {
-    exec(success, error, 'JedlixPlugin', 'chargerMethod', [url, apikey, userid, accesstoken, charginglocationid]);
+exports.chargerMethod = function(url, apikey, userid, accesstoken, charginglocationid, opensessionid, success, error) {
+    exec(success, error, 'JedlixPlugin', 'chargerMethod', [url, apikey, userid, accesstoken, charginglocationid, opensessionid]);
 };
